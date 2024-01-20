eaf3def1f0c61b97c9e78accb95264453f8b3299
JianJiaT <tjjtjn@gmail.com>
2024-01-20 13:06:02 +0800

    Implement Dor.java skeletal code

--- Dor.java
+++ Dor.java
@@ -1,10 +1,7 @@
 public class Dor {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Dor");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }

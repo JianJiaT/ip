8e5db6f511517902b20273dcddb8c03d5f261292
JianJiaT <tjjtjn@gmail.com>
2024-01-27 22:48:10 +0800

    Implement Task class

--- Task.java
+++ Task.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task() {
+        this.name = "placeholder";
+        this.isDone = false;
+    }
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDoneStatus() {
+        if (isDone) {
+            return "X";
+        }
+        return " ";
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
 }

35a8465109a8743311a87def9679af460a9bed7c
JianJiaT <tjjtjn@gmail.com>
2024-01-27 18:37:12 +0800

    Add echo functionality

--- Dor.java
+++ Dor.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class Dor {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Dor");
         System.out.println("What can I do for you?");
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
